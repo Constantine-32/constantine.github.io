fe40f55697c4c308e97c230065afdd754e4766fd
Christian C. Romero <id.32@outlook.com>
2017-09-30 18:11:59 +0200

    Full screen clock

--- clock/clock.js
+++ clock/clock.js
@@ -1,12 +1,27 @@
-function GetClock() {
-  var date = new Date();
-  var hour = date.getHours(), min = date.getMinutes(), sec = date.getSeconds()
-  if (min <= 9) min = "0" + min
-  if (sec <= 9) sec = "0" + sec
-  document.getElementById('clock').innerHTML = hour + " : " + min + " : " + sec
+var textElem = document.getElementById("clock");
+var textNode = document.createTextNode("");
+textElem.appendChild(textNode);
+
+var targetWidth = 0.8;  // Proportion of full screen width
+var curFontSize = 20;  // Do not change
+
+function updateClock() {
+  var d = new Date();
+  var s = "";
+  s += (d.getHours  () < 10 ? "0" : "") + d.getHours  () + ":";
+  s += (d.getMinutes() < 10 ? "0" : "") + d.getMinutes() + ":";
+  s += (d.getSeconds() < 10 ? "0" : "") + d.getSeconds();
+  textNode.data = s;
+  setTimeout(updateClock, 1000 - d.getTime() % 1000 + 20);
 }
 
-window.onload = function () {
-  GetClock()
-  setInterval(GetClock, 1000)
+function updateTextSize() {
+  for (var i = 0; i < 4; i++) {  // Iterate for better better convergence
+    curFontSize *= targetWidth / (textElem.offsetWidth / textElem.parentNode.offsetWidth);
+    textElem.style.fontSize = curFontSize + "pt";
+  }
 }
+
+updateClock();
+updateTextSize();
+window.addEventListener("resize", updateTextSize);
