9a6eb283bf3042b42f38a674982cb0855bad2b76
Christian C. Romero <id.32@outlook.com>
2017-10-12 05:07:49 +0200

    js code refactoring v2 (100x times better than before xD)

--- unitconverters/time.js
+++ unitconverters/time.js
@@ -1,87 +1,11 @@
-var ELEMENT = [document.getElementById("seconds"),
-                 document.getElementById("minutes"),
-                 document.getElementById("hours"),
-                 document.getElementById("days"),
-                 document.getElementById("weeks"),
-                 document.getElementById("months"),
-                 document.getElementById("years"),
-                 document.getElementById("humans"),
-                 document.getElementById("earth"),
-                 document.getElementById("universe")]
+function updateValues(id) {
+  const ELE = document.getElementsByClassName('unit_input')
+  const RATIOS = [1, 60, 3600, 86400, 604800, 2628000, 31536000, 2491344000, 143173440000000000, 435165264000000000]
 
-function hideZeros() {
-  for (var i = 0; i <= ELEMENT.length; i++) {
-    if (ELEMENT[i].value === "0") ELEMENT[i].value = ""
+  if (!isNaN(ELE[id].value) && ELE[id].value >= 0) ELE[0].value = ELE[id].value * RATIOS[id]
+
+  for (let i = 0; i < RATIOS.length; i++) {
+    ELE[i].value = Math.round(ELE[0].value / RATIOS[i] * 10) / 10
+    if (ELE[i].value === '0') ELE[i].value = ''
   }
-}
-
-function formatNum(number) {
-  return Math.floor(number)
-}
-
-function updateAll() {
-  ELEMENT[1].value = formatNum(ELEMENT[0].value / 60)
-  ELEMENT[2].value = formatNum(ELEMENT[0].value / 3600)
-  ELEMENT[3].value = formatNum(ELEMENT[0].value / 86400)
-  ELEMENT[4].value = formatNum(ELEMENT[0].value / 604800)
-  ELEMENT[5].value = formatNum(ELEMENT[0].value / 2628000)
-  ELEMENT[6].value = formatNum(ELEMENT[0].value / 31536000)
-  ELEMENT[7].value = formatNum(ELEMENT[0].value / 2491344000)
-  ELEMENT[8].value = formatNum(ELEMENT[0].value / 143173440000000000)
-  ELEMENT[9].value = formatNum(ELEMENT[0].value / 435165264000000000)
-  hideZeros()
-}
-
-function updateMinutes() {
-  var ele = ELEMENT[1]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 60)
-  updateAll()
-}
-
-function updateHours() {
-  var ele = ELEMENT[2]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 3600)
-  updateAll()
-}
-
-function updateDays() {
-  var ele = ELEMENT[3]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 86400)
-  updateAll()
-}
-
-function updateWeeks() {
-  var ele = ELEMENT[4]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 604800)
-  updateAll()
-}
-
-function updateMonths() {
-  var ele = ELEMENT[5]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 2628000)
-  updateAll()
-}
-
-function updateYears() {
-  var ele = ELEMENT[6]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 31536000)
-  updateAll()
-}
-
-function updateHumans() {
-  var ele = ELEMENT[7]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 2491344000)
-  updateAll()
-}
-
-function updateEarth() {
-  var ele = ELEMENT[8]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 143173440000000000)
-  updateAll()
-}
-
-function updateUniverse() {
-  var ele = ELEMENT[9]
-  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 435165264000000000)
-  updateAll()
 }
