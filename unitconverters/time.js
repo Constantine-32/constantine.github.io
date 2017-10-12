2482773a1174b017e055915faa945c1e4d542016
Christian C. Romero <id.32@outlook.com>
2017-10-12 04:47:00 +0200

    js code refactoring v1

--- unitconverters/time.js
+++ unitconverters/time.js
@@ -1,88 +1,87 @@
-function getElements() {
-  return [document.getElementById("seconds"),
-          document.getElementById("minutes"),
-          document.getElementById("hours"),
-          document.getElementById("days"),
-          document.getElementById("weeks"),
-          document.getElementById("months"),
-          document.getElementById("years"),
-          document.getElementById("humans"),
-          document.getElementById("earth"),
-          document.getElementById("universe")]
-}
+var ELEMENT = [document.getElementById("seconds"),
+                 document.getElementById("minutes"),
+                 document.getElementById("hours"),
+                 document.getElementById("days"),
+                 document.getElementById("weeks"),
+                 document.getElementById("months"),
+                 document.getElementById("years"),
+                 document.getElementById("humans"),
+                 document.getElementById("earth"),
+                 document.getElementById("universe")]
 
-function updateElements(elements) {
-  for (var i = 0; i <= elements.length; i++) {
-    if (elements[i].value === "0") {
-      elements[i].value = ""
-    }
+function hideZeros() {
+  for (var i = 0; i <= ELEMENT.length; i++) {
+    if (ELEMENT[i].value === "0") ELEMENT[i].value = ""
   }
 }
 
-function updateSeconds() {
-  var ele = getElements()
-  ele[1].value = Math.floor(ele[0].value / 60)
-  ele[2].value = Math.floor(ele[0].value / 3600)
-  ele[3].value = Math.floor(ele[0].value / 86400)
-  ele[4].value = Math.floor(ele[0].value / 604800)
-  ele[5].value = Math.floor(ele[0].value / 2628000)
-  ele[6].value = Math.floor(ele[0].value / 31536000)
-  ele[7].value = Math.floor(ele[0].value / 2491344000)
-  ele[8].value = Math.floor(ele[0].value / 143173440000000000)
-  ele[9].value = Math.floor(ele[0].value / 435165264000000000)
-  updateElements(ele)
+function formatNum(number) {
+  return Math.floor(number)
+}
+
+function updateAll() {
+  ELEMENT[1].value = formatNum(ELEMENT[0].value / 60)
+  ELEMENT[2].value = formatNum(ELEMENT[0].value / 3600)
+  ELEMENT[3].value = formatNum(ELEMENT[0].value / 86400)
+  ELEMENT[4].value = formatNum(ELEMENT[0].value / 604800)
+  ELEMENT[5].value = formatNum(ELEMENT[0].value / 2628000)
+  ELEMENT[6].value = formatNum(ELEMENT[0].value / 31536000)
+  ELEMENT[7].value = formatNum(ELEMENT[0].value / 2491344000)
+  ELEMENT[8].value = formatNum(ELEMENT[0].value / 143173440000000000)
+  ELEMENT[9].value = formatNum(ELEMENT[0].value / 435165264000000000)
+  hideZeros()
 }
 
 function updateMinutes() {
-  var ele = document.getElementById("minutes")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 60)
-  updateSeconds()
+  var ele = ELEMENT[1]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 60)
+  updateAll()
 }
 
 function updateHours() {
-  var ele = document.getElementById("hours")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 3600)
-  updateSeconds()
+  var ele = ELEMENT[2]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 3600)
+  updateAll()
 }
 
 function updateDays() {
-  var ele = document.getElementById("days")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 86400)
-  updateSeconds()
+  var ele = ELEMENT[3]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 86400)
+  updateAll()
 }
 
 function updateWeeks() {
-  var ele = document.getElementById("weeks")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 604800)
-  updateSeconds()
+  var ele = ELEMENT[4]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 604800)
+  updateAll()
 }
 
 function updateMonths() {
-  var ele = document.getElementById("months")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 2628000)
-  updateSeconds()
+  var ele = ELEMENT[5]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 2628000)
+  updateAll()
 }
 
 function updateYears() {
-  var ele = document.getElementById("years")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 31536000)
-  updateSeconds()
+  var ele = ELEMENT[6]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 31536000)
+  updateAll()
 }
 
 function updateHumans() {
-  var ele = document.getElementById("humans")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 2491344000)
-  updateSeconds()
+  var ele = ELEMENT[7]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 2491344000)
+  updateAll()
 }
 
 function updateEarth() {
-  var ele = document.getElementById("earth")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 143173440000000000)
-  updateSeconds()
+  var ele = ELEMENT[8]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 143173440000000000)
+  updateAll()
 }
 
 function updateUniverse() {
-  var ele = document.getElementById("universe")
-  if (!isNaN(ele.value) && ele.value >= 0) document.getElementById("seconds").value = Math.floor(ele.value * 435165264000000000)
-  updateSeconds()
+  var ele = ELEMENT[9]
+  if (!isNaN(ele.value) && ele.value >= 0) ELEMENT[0].value = formatNum(ele.value * 435165264000000000)
+  updateAll()
 }
