bbb0ed5219d7f166064b45c056f140e7fb346f05
Christian C. Romero <id.32@outlook.com>
2018-12-18 21:30:59 +0100

    replaced dodecahedron by icosahedron

--- sketch.js
+++ sketch.js
@@ -21,8 +21,58 @@ function draw() {
   strokeWeight(2)
   rotateX(frameCount *  0.01)
   rotateY(frameCount *  0.01)
-  rotateZ(frameCount *  0.01)
-  dodecahedron(80)
+  // rotateZ(frameCount *  0.01)
+  icosahedron(80)
+}
+
+function icosahedron(r) {
+  const p = r * 1.618033988749895
+  const z1 = [ 0,  p,  r]
+  const z2 = [ 0,  p, -r]
+  const z3 = [ 0, -p,  r]
+  const z4 = [ 0, -p, -r]
+  const x1 = [ r,  0,  p]
+  const x2 = [-r,  0,  p]
+  const x3 = [ r,  0, -p]
+  const x4 = [-r,  0, -p]
+  const y1 = [ p,  r,  0]
+  const y2 = [ p, -r,  0]
+  const y3 = [-p,  r,  0]
+  const y4 = [-p, -r,  0]
+
+  line(...z1, ...z2)
+  line(...z3, ...z4)
+  line(...x1, ...x2)
+  line(...x3, ...x4)
+  line(...y1, ...y2)
+  line(...y3, ...y4)
+
+  line(...z1, ...x1)
+  line(...z1, ...x2)
+  line(...z2, ...x3)
+  line(...z2, ...x4)
+  line(...z3, ...x1)
+  line(...z3, ...x2)
+  line(...z4, ...x3)
+  line(...z4, ...x4)
+
+  line(...y1, ...z1)
+  line(...y1, ...z2)
+  line(...y2, ...z3)
+  line(...y2, ...z4)
+  line(...y3, ...z1)
+  line(...y3, ...z2)
+  line(...y4, ...z3)
+  line(...y4, ...z4)
+
+  line(...x1, ...y1)
+  line(...x1, ...y2)
+  line(...x2, ...y3)
+  line(...x2, ...y4)
+  line(...x3, ...y1)
+  line(...x3, ...y2)
+  line(...x4, ...y3)
+  line(...x4, ...y4)
 }
 
 function dodecahedron(r) {
