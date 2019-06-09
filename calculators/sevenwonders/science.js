08d488ee5253a99bee84eef91b6d5c4264575c3c
Christian C. Romero <id.32@outlook.com>
2019-06-09 06:03:18 +0200

    Sevenwonders science calculator

---
+++ calculators/sevenwonders/science.js
@@ -0,0 +1,39 @@
+const rules = document.getElementById('rules')
+const stone = document.getElementById('stone')
+const gears = document.getElementById('gears')
+const score = document.getElementById('score')
+const rulesI = document.getElementById('rules-icon')
+const stoneI = document.getElementById('stone-icon')
+const gearsI = document.getElementById('gears-icon')
+const rulesP = document.getElementById('rules-plus')
+const stoneP = document.getElementById('stone-plus')
+const gearsP = document.getElementById('gears-plus')
+
+const getScore = (a, b, c) => a*a + b*b + c*c + 7*Math.min(a, b, c)
+const getInt = (e) => e.value !== '' ? parseInt(e.value) : 0
+const getVal = (e) => [1, 0, -1][e.button]
+
+const calc = (ele) => {
+  if (isNaN(ele.value) || ele.value < 1 || ele.value > 99)
+    ele.value = ''
+  const rulesV = getInt(rules)
+  const stoneV = getInt(stone)
+  const gearsV = getInt(gears)
+  score.value = getScore(rulesV, stoneV, gearsV)
+  rulesP.innerHTML = '+' + (getScore(rulesV+1, stoneV, gearsV) - score.value)
+  stoneP.innerHTML = '+' + (getScore(rulesV, stoneV+1, gearsV) - score.value)
+  gearsP.innerHTML = '+' + (getScore(rulesV, stoneV, gearsV+1) - score.value)
+}
+
+rules.addEventListener('keyup', () => { calc(rules) })
+stone.addEventListener('keyup', () => { calc(stone) })
+gears.addEventListener('keyup', () => { calc(gears) })
+
+rules.addEventListener('click', () => { rules.select() })
+stone.addEventListener('click', () => { stone.select() })
+gears.addEventListener('click', () => { gears.select() })
+score.addEventListener('click', () => { score.select() })
+
+rulesI.addEventListener('mouseup', (e) => { rules.value = getInt(rules) + getVal(e); calc(rules) })
+stoneI.addEventListener('mouseup', (e) => { stone.value = getInt(stone) + getVal(e); calc(stone) })
+gearsI.addEventListener('mouseup', (e) => { gears.value = getInt(gears) + getVal(e); calc(gears) })
