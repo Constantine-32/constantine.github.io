e6da15a7bf91eca8872647192cecfd7d4a61f616
Christian C. Romero <id.32@outlook.com>
2020-04-09 17:56:59 +0200

    added spades and actual dig level support

--- calculators/tm/terra.js
+++ calculators/tm/terra.js
@@ -17,19 +17,21 @@ $(function () {
   }
 
   const vs = {
-    'op': 0, 'odw': 0, 'otp': 0, 'otpp': 0, 'ote': 0, 'osa': 0, 'osh': 0, 'odig': 0, 'oship': 0,
-    'aw': 0, 'ac': 0, 'ap': 0, 'iw': 0, 'ic': 0, 'ip': 0, 'bw': 0, 'bc': 0, 'bp': 0
+    'odw': 0, 'otp': 0, 'otpp': 0, 'ote': 0, 'osa': 0, 'osh': 0, 'ospd': 0, 'odig': 0, 'oship': 0,
+    'aw': 0, 'ac': 0, 'ap': 0, 'adig': 0, 'iw': 0, 'ic': 0, 'ip': 0, 'bw': 0, 'bc': 0, 'bp': 0
   }
 
-  const digButton = $('#odig')
-  const digText = $('#odig-text')
+  const odigButton = $('#odig')
+  const odigText = $('#odig-text')
+  const adigButton = $('#adig')
+  const adigText = $('#adig-text')
   const shipButton = $('#oship')
   const shipText = $('#oship-text')
   const bwText = $("#bw-text")[0]
   const bcText = $('#bc-text')[0]
   const bpText = $('#bp-text')[0]
 
-  let race = 'Darklings'
+  let race = 'Alchemists'
 
   $('.race-button').each(function (_, element) {
     element.addEventListener('click', function (event) {
@@ -39,15 +41,21 @@ $(function () {
         $('#'+newRace).addClass('selected')
 
         if (race === 'Darklings') {
-          digButton.removeClass('disabled')
-          digText.removeClass('disabled')
-          digText.prop('disabled', false)
+          odigButton.removeClass('disabled')
+          odigText.removeClass('disabled')
+          odigText.prop('disabled', false)
+          adigButton.removeClass('disabled')
+          adigText.removeClass('disabled')
+          adigText.prop('disabled', false)
         }
 
         if (newRace === 'Darklings') {
-          digButton.addClass('disabled')
-          digText.addClass('disabled')
-          digText.prop('disabled', true)
+          odigButton.addClass('disabled')
+          odigText.addClass('disabled')
+          odigText.prop('disabled', true)
+          adigButton.addClass('disabled')
+          adigText.addClass('disabled')
+          adigText.prop('disabled', true)
         }
 
         if (race === 'Fakirs' || race === 'Dwarves') {
@@ -73,7 +81,20 @@ $(function () {
       if (Array.from(event.target.classList).includes('disabled')) return
       const id = event.target.id
       if (vs[id] === 99) return
-      vs[id]++
+
+      switch (id) {
+        case 'osa':
+        case 'osh':
+          if (vs[id] === 0) vs[id]++
+          break;
+        case 'odig':
+        case 'adig':
+          if ((vs['odig'] + vs['adig'] < 2)) vs[id]++
+          break;
+        default:
+          vs[id]++
+      }
+
       $('#'+id+'-text')[0].value = vs[id]
       recalc()
     })
@@ -117,7 +138,7 @@ $(function () {
   function recalc() {
     vs['bw'] = vs['aw'] + vs['iw']
     vs['bc'] = vs['ac'] + vs['ic']
-    vs['bp'] = vs['ap'] + vs['ip'] - vs['op']
+    vs['bp'] = vs['ap'] + vs['ip']
 
     // Std structures
     if (race === 'Swarmlings') {
@@ -131,6 +152,13 @@ $(function () {
     // Specific structures
     vs['bw'] -= vs['osa'] * dd[race]['sa'][0] + vs['osh'] * dd[race]['sh'][0]
     vs['bc'] -= vs['osa'] * dd[race]['sa'][1] + vs['osh'] * dd[race]['sh'][1]
+
+    // Spades
+    if (race === 'Darklings') {
+      vs['bp'] -= vs['ospd']
+    } else {
+      vs['bw'] -= vs['ospd'] * (3 - vs['adig'] - vs['odig'])
+    }
 
     // Dig
     if (race !== 'Darklings') {
